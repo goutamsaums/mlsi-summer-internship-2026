d81c8d7e687bd2b928ae8559b0653c4469102c41
GOUTAM ANAND <goutam@students.sau.ac.in>
2026-05-26 21:56:16 +0530

    Day 1: Expanded NumPy practice examples

--- Day1_NumPy/basic_arrays.py
+++ Day1_NumPy/basic_arrays.py
@@ -3,9 +3,13 @@ import numpy as np
 # creating arrays
 
 a = np.array([1, 2, 3, 4])
+
 b = np.zeros((2,2))
+
 c = np.ones((2,2))
+
 d = np.arange(0, 10, 2)
+
 e = np.linspace(0, 1, 5)
 
 print("Array:")
@@ -23,6 +27,14 @@ print(d)
 print("\nLinspace:")
 print(e)
 
+# indexing and slicing
+
+print("\nFirst Element:")
+print(a[0])
+
+print("\nSlicing:")
+print(a[1:3])
+
 # basic operations
 
 print("\nAddition:")
@@ -31,7 +43,15 @@ print(a + 2)
 print("\nMultiplication:")
 print(a * 2)
 
-# transpose
+# statistical operations
+
+print("\nMean:")
+print(np.mean(a))
+
+print("\nStandard Deviation:")
+print(np.std(a))
+
+# transpose operation
 
 x = np.array([[1,2],[3,4]])
 
@@ -44,3 +64,8 @@ v = np.array([1,0])
 
 print("\nBroadcasting:")
 print(x + v)
+
+# random array
+
+print("\nRandom Array:")
+print(np.random.rand(2,2))
